1b0d1b8d8c9292764eafbb86f9f4ec9c249ecec4
Rich <rclt201@gmail.com>
2020-06-21 10:45:07 +0100

    changed logic for view height

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,40 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { InputsAndRatings } from './InputsAndRatings'
 import { Result } from './Result'
-import { useEventListener, useDebounce } from '../util'
 import { colorScheme } from '../static'
 import { Icon } from './common'
 
 export const Home = () => {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  const [vh, setVh] = useState(window.innerHeight * 0.01)
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-  const handleResize = () => {
-    console.log('in handle resize')
-    // We execute the same script as before
-    setVh(window.innerHeight * 0.01)
-  }
-
-  const deboundedVh = useDebounce(vh, 400)
-
-  useEffect(() => {
-    // Make sure we have a deboundedVh
-    if (deboundedVh) {
-      // Fire off our function
-      document.documentElement.style.setProperty('--vh', `${deboundedVh}px`)
-    }
-  }, [deboundedVh])
-
-  useEventListener('resize', handleResize, window)
-  useEventListener('orientationchange', handleResize, window)
-
   const [displayResult, setDisplayResult] = useState<boolean>(false)
   const [activeUserNumber, setActiveUserNumber] = useState<1 | 2>(1)
 
   return (
-    <div style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
+    <div
+      style={{
+        height: window.innerHeight,
+      }}
+    >
       <div className='hidden h-full lg:block'>
         <div className='p-5 text-center'>
           Movie Matcher is currently only available on mobile
